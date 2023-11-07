4107450794a57853ecba6333879d7e87f4a8677d
Carlos <c.pena17@ufromail.cl>
2023-11-07 16:28:05 -0300

    folder rename

---
+++ la-tienda-del-vinilo-frontend/src/views/About.vue
@@ -0,0 +1,85 @@
+<template>
+    <main class="about-page">
+
+        <Searchbar />
+
+        <ProfileButton style="display: flex; justify-content: end;"></ProfileButton>
+
+        <div class="text-center">
+            <h1>Contactanos</h1>
+        </div>
+
+
+        <v-layout class="rounded rounded-md">
+
+            <v-main class="d-flex align-center" style="min-height: 500px;">
+                <v-sheet elevation="12" max-width="600" rounded="lg" width="100%" class="pa-4 text-center mx-auto">
+                    <v-icon class="mb-5" color="#520100" icon="mdi-account" size="112"></v-icon>
+
+                    <h2 class="text-h5 mb-6">Acerca de nosotros</h2>
+                    <p>Entra a la tienda del vinilo! y encuentra las mejores ofertas en los catalogos de nuestra tienda</p>
+
+                    <div class="about-item align-center py-3">
+
+                        <v-icon class="mb-3" icon="mdi-cellphone" size="35">
+                        </v-icon>
+
+                        <p>+564 5247 8965</p>
+
+                    </div>
+                    <div class="about-item inline align-center py-3">
+
+                        <v-icon class="mb-3" icon="mdi-map-marker" size="35">
+                        </v-icon>
+
+                        <p>Av.Alemania 0671, Temuco</p>
+
+                    </div>
+
+
+
+
+                    <v-divider class="mb-4"></v-divider>
+
+                    <div class="text-end">
+                        <v-btn class="text-none" color="#22c55e" rounded variant="flat">
+                            <p> Tu opnion nos importa
+                            </p>
+                        </v-btn>
+                    </div>
+                </v-sheet>
+            </v-main>
+
+
+
+        </v-layout>
+
+
+    </main>
+</template>
+<script setup>
+import Searchbar from '../components/Searchbar.vue';
+import ProfileButton from '../components/ProfileButton.vue';
+</script>
+
+<style lang="scss">
+.footer-style {
+
+    border: 1px solid red;
+    height: 200px;
+    width: 100rem;
+}
+
+.container {
+    display: flex;
+    border: 1px solid red;
+    align-items: center;
+    justify-content: center;
+}
+
+
+.about-item {
+    display: flex;
+    align-content: center;
+}
+</style>
---
+++ la-tienda-del-vinilo-frontend/src/views/Home.vue
@@ -0,0 +1,144 @@
+<template>
+    <main class="home-page">
+        <Searchbar />
+        <div class="item">
+            <h1>Vinilos</h1>
+            <CategoryButton class="CButton" />
+            <ProfileButton />
+
+        </div>
+        <v-divider class="mb-4"></v-divider>
+
+        <v-carousel cycle hide-delimiters show-arrows="hover">
+            <v-carousel-item v-for="datas in data">
+                <v-sheet class="card-content " border rounded elevation="10">
+
+                    <v-row class="car">
+
+                        <v-col cols="5" style="height: 20rem;">
+                            <v-img src="../assets/logo.png"></v-img>
+
+                        </v-col>
+                        <v-col cols="7" style="height: fit-content;">
+
+                            <h1>{{ datas.name }}</h1>
+
+                            <v-rating :model-value="datas.rating" color="amber" density="compact" half-increments readonly
+                                size="small"></v-rating>
+                            <br>
+                            <p>
+                                {{ datas.description }}
+
+
+                            </p>
+
+                            <br>
+                            <h4>
+                                $ {{ datas.price }}
+                            </h4>
+
+
+                        </v-col>
+
+                        <v-sheet class="d-flex" style="width: 60rem;">
+                            <v-sheet class="ma-2 pa-2 me-auto">
+
+                                <v-btn text="Añadir al Carro" color="#520100" variant="tonal"
+                                    @click="cartStore.add(datas.id)"></v-btn>
+                            </v-sheet>
+                            <v-sheet class="ma-2 pa-2">
+                                <v-btn color="#520100" variant="tonal"
+                                    @click="wishlistStore.add(datas.id)">
+                                <v-icon>mdi-heart</v-icon>
+                                </v-btn>
+                            </v-sheet>
+                            <v-sheet class="ma-2 pa-2">
+                                <v-btn text="Comprar"></v-btn>
+                            </v-sheet>
+
+
+                        </v-sheet>
+
+
+
+                    </v-row>
+
+                </v-sheet>
+            </v-carousel-item>
+
+        </v-carousel>
+        <v-divider class="mb-4"></v-divider>
+
+        <v-sheet class="d-flex flex-wrap ">
+            <Card v-for="datas in data" :id="datas.id" :name="datas.name" :description="datas.description"
+                :rating="datas.rating" :price="datas.price"></Card>
+
+        </v-sheet>
+
+
+
+
+
+    </main>
+</template>
+
+<script setup>
+import Searchbar from '../components/Searchbar.vue';
+import ProfileButton from '@/components/ProfileButton.vue';
+import Card from '../components/Card.vue';
+import data from '../data/data.js';
+import CategoryButton from '../components/CategoryButton.vue'
+import { useCartStore } from '../stores/CartStore'
+import { useWishlistStore } from '@/stores/WishlistStore';
+const cartStore = useCartStore();
+
+const wishlistStore = useWishlistStore();
+
+console.log(wishlistStore.formattedList);
+
+</script>
+
+<style lang="scss">
+.buttons {
+    margin-right: 4rem;
+    padding-bottom: 1rem;
+    justify-content: space-between;
+}
+
+
+.car {
+    display: flex;
+    align-items: center;
+    justify-content: end;
+    margin-right: 4rem;
+
+}
+
+.card-content {
+    padding-top: 20px;
+    padding-right: 20px;
+    padding-bottom: 20px;
+    padding-left: 20px;
+
+}
+
+.center-card {
+    padding-top: 100px;
+    padding-right: 50px;
+    padding-bottom: 100px;
+    padding-left: 50px;
+}
+
+.item {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+
+.CButton {
+    display: flex;
+
+    align-self: center;
+
+}
+</style>
