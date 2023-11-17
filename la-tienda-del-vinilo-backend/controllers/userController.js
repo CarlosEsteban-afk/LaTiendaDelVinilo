401c3ba0cc991467fabbea28272a6e7385d8b5f0
Carlos <c.pena17@ufromail.cl>
2023-11-17 15:37:07 -0300

    fix "}"

--- la-tienda-del-vinilo-backend/controllers/userController.js
+++ la-tienda-del-vinilo-backend/controllers/userController.js
@@ -30,16 +30,16 @@ exports.addToWishlist = async (req, res) => {
 
         const result = await User.findOne({ rut });
         console.log(result);
-        
+
         try {
-            if(Product.find({ id })){
-               result.wishlistItems.push({ id, name, stock, price, description, category, rating, imgUrl });
+            if (Product.find({ id })) {
+                result.wishlistItems.push({ id, name, stock, price, description, category, rating, imgUrl });
             }
             else {
-                const found = await Product.find({id});
-               result.wishlistItems.pop(found);
+                const found = await Product.find({ id });
+                result.wishlistItems.pop(found);
             }
-          result.save();   
+            result.save();
         } catch (error) {
             console.log(error);
         }
@@ -73,20 +73,20 @@ exports.getUserWishlist = async (req, res) => {
 }
 
 exports.removeFromWishlist = async (req, res) => {
-        const { rut } = req.params;
-        const { id } = req.body
+    const { rut } = req.params;
+    const { id } = req.body
 
-        const result = await User.findOne({ rut });
-        console.log(result);
-        try{
-            const deletedProduct = await Product.findOneAndDelete({ id });
-            if (!deletedProduct) {
-                return res.status(404).send('Producto no encontrado');
-            }
-            res.status(204).send('Producto eliminado');
-        } catch(error) {
-            res.status(500).send('Error al eliminar el producto');
+    const result = await User.findOne({ rut });
+    console.log(result);
+    try {
+        const deletedProduct = await Product.findOneAndDelete({ id });
+        if (!deletedProduct) {
+            return res.status(404).send('Producto no encontrado');
         }
+        res.status(204).send('Producto eliminado');
+    } catch (error) {
+        res.status(500).send('Error al eliminar el producto');
+    }
 };
 
 exports.createUser = async (req, res) => {
@@ -98,50 +98,51 @@ exports.createUser = async (req, res) => {
         res.status(201).json(user);
     } catch (error) {
         res.status(500).send({ error: 'Internal server error' });
-};
+    };
+}
 
 
 
-exports.loginUser = async (req, res) =>{
-    const userName = req.body.username;
-    const userPassword = req.body.password;
-    try {
-        const loginUser = await User.find({ username: userName,password: userPassword });
-        //si el array es 0 es por que no existe
-        if (loginUser.length==0) {
-            return res.status(404).send('usuario no encontrado');
+    exports.loginUser = async (req, res) => {
+        const userName = req.body.username;
+        const userPassword = req.body.password;
+        try {
+            const loginUser = await User.find({ username: userName, password: userPassword });
+            //si el array es 0 es por que no existe
+            if (loginUser.length == 0) {
+                return res.status(404).send('usuario no encontrado');
+            }
+            res.status(202).json(loginUser);
+
+        } catch (error) {
+            console.error('Error al ingresar el usuario:', error);
+            res.status(500).send('Error al ingresar el usuario');
         }
-        res.status(202).json(loginUser);
-        
-    } catch (error) {
-        console.error('Error al ingresar el usuario:', error);
-        res.status(500).send('Error al ingresar el usuario');
-    }
-};
+    };
 
-exports.updateUser = async (req,res)=>{
-    const userId = parseInt(req.body.id);
-    try{
-        const updatedUser = await User.findOneAndUpdate({id: userId},
-            {
-                username: req.body.username,
-                password: req.body.password,
-                email: req.body.email,
-                name: req.body.name,
-                lastname: req.body.lastname
-            },{
+    exports.updateUser = async (req, res) => {
+        const userId = parseInt(req.body.id);
+        try {
+            const updatedUser = await User.findOneAndUpdate({ id: userId },
+                {
+                    username: req.body.username,
+                    password: req.body.password,
+                    email: req.body.email,
+                    name: req.body.name,
+                    lastname: req.body.lastname
+                }, {
                 new: true
             });
-        if (!updatedUser) {
-          res.status(404).send('usuario no encontrado');
-        } 
-        res.json(updatedUser);
-    }catch(error){
-        console.error('Error al actualizar el usuario:', error);
-        res.status(500).send('Error al actualizar el usuario');
+            if (!updatedUser) {
+                res.status(404).send('usuario no encontrado');
+            }
+            res.json(updatedUser);
+        } catch (error) {
+            console.error('Error al actualizar el usuario:', error);
+            res.status(500).send('Error al actualizar el usuario');
 
-    }
-};
+        }
+    };
 
 
 
